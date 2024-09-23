edcd0fa7c5a0d6720392f4ba46d01a09847bb098
fajarmarif22 <fmaulanaarif@gmail.com>
2024-09-23 14:47:52 +0700

    First Commits

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+  <>
+  <div className="flex">
+    <div>About Me</div>
+    <div>Project</div>
+  </div>
+  </>)
+}
+
+export default Header;
